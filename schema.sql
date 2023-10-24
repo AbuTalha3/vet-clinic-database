c1574bf26df102b7879ecc9e93df134f86f52158
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-25 03:25:14 +0500

    Amend schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer primary key auto_increment,
+    id integer,
     name varchar(100),
     date_of_birth date,
     escape_atttempts decimal(10,2),
