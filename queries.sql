3c9f63c3174c3e3d8f028d42440c717b0ae2d8ed
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-27 18:38:38 +0500

    Add Queries to search Tables

--- queries.sql
+++ queries.sql
@@ -8,3 +8,35 @@
  SELECT * FROM animals WHERE neutered = TRUE;
  SELECT * FROM animals WHERE name <> 'Gabumon';
  SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3; 
+
+SELECT a.name AS animal_name FROM owners o
+JOIN animals a ON o.id = a.owner_id
+WHERE o.full_name = 'Melody Pond';
+
+SELECT a.name AS animal_name FROM animals a
+JOIN species s ON a.species_id = s.id
+WHERE s.name = 'Pokemon';
+
+SELECT o.full_name AS owner_name, a.name AS animal_name
+FROM owners o
+LEFT JOIN animals a ON o.id = a.owner_id;
+
+SELECT s.name AS species_name, COUNT(*) AS animal_count FROM species s
+LEFT JOIN animals a ON s.id = a.species_id
+GROUP BY s.name ORDER BY s.name;
+
+SELECT a.name AS Digimon_name FROM owners o
+JOIN animals a ON o.id = a.owner_id
+JOIN species s ON a.species_id = s.id
+WHERE o.full_name = 'Jennifer Orwell' AND s.name = 'Digimon';
+
+SELECT a.name AS animal_name  FROM owners o
+JOIN animals a ON o.id = a.owner_id
+WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
+
+SELECT o.full_name AS owner_name, COUNT(a.id) AS animal_count
+FROM owners o
+LEFT JOIN animals a ON o.id = a.owner_id
+GROUP BY o.full_name
+ORDER BY animal_count DESC
+LIMIT 1;
