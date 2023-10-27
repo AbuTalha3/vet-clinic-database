5eaf03ffe2857c586fa340d98a09f4d34724891c
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-27 23:16:38 +0500

    Add Queries and answers

--- queries.sql
+++ queries.sql
@@ -8,3 +8,58 @@
  SELECT * FROM animals WHERE neutered = TRUE;
  SELECT * FROM animals WHERE name <> 'Gabumon';
  SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3; 
+
+<--Queries for 3rd day-->
+
+SELECT a.name 
+FROM animals a JOIN visits v ON v.animal_id = a.id 
+JOIN vets ve ON ve.id = v.vet_id 
+WHERE ve.name = 'William Tatcher' 
+ORDER BY v.date_of_visit DESC
+LIMIT 1;
+
+SELECT COUNT(DISTINCT a.id) AS num_animals_seen
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets ve ON v.vet_id = ve.id
+WHERE ve.name = 'Stephanie Mendez';
+
+SELECT ve.name, s.name 
+from vets ve LEFT JOIN specializations spe ON spe.vet_id = ve.id 
+LEFT JOIN species s ON s.id = spe.species_id;
+
+SELECT a.name, COUNT(*) AS num_visits
+FROM visits v
+JOIN animals a ON v.animal_id = a.id
+GROUP BY a.id
+ORDER BY num_visits DESC
+LIMIT 1;
+
+SELECT count(v.id) from visits v
+JOIN animals a ON v.animal_id = a.id
+JOIN vets ve ON v.vet_id = ve.id
+LEFT JOIN specializations spe ON spe.vet_id = ve.id AND spe.species_id = a.species_id
+WHERE spe.id IS NULL;
+
+SELECT s.name FROM animals a
+JOIN species s ON a.species_id = s.id
+JOIN visits v ON v.animal_id = a.id
+JOIN vets ve ON ve.id = v.vet_id
+where ve.name = 'Maisy Smith'
+GROUP BY s.name
+ORDER BY count(a.id) DESC;
+
+SELECT a.name, v.date_of_visit 
+FROM animals a 
+JOIN visits v ON v.animal_id = a.id 
+JOIN vets ve ON ve.id = v.vet_id 
+WHERE ve.name = 'Stephanie Mendez' AND v.date_of_visit 
+BETWEEN '2020-04-01' AND '2020-08-30';
+
+SELECT a.name 
+FROM animals a 
+JOIN visits v ON v.animal_id = a.id 
+JOIN vets ve ON ve.id = v.vet_id 
+WHERE ve.name = 'Maisy Smith' 
+ORDER BY v.date_of_visit ASC
+LIMIT 1;
